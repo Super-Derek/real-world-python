bcca48b218927a2b252fd65753578311365d70a6
Derek Lewis <69864186+Super-Derek@users.noreply.github.com>
2025-11-14 12:38:49 -0500

    Ensure Chapter 1 dependencies are installed in loval venv

--- ch01_saving_shipwrecked_sailors_with_bayes_rule/src/search_and_rescue/main.py
+++ ch01_saving_shipwrecked_sailors_with_bayes_rule/src/search_and_rescue/main.py
@@ -4,6 +4,12 @@ Base implementation of the Bayesian search-and-rescue model
 from 'Real World Python' by Lee Vaughan, Chapter 1.
 """
 
+import sys
+import random
+import itertools
+import numpy as np
+import cv2 as cv
+
 def main():
     print("Search and Rescue simulation starting...")
 
